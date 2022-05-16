9a83aafbc0c41f171d409e440d906263642384b7
Moreno <moreno.livak@gmail.com>
2022-05-16 16:14:33 +0200

    main

--- dist/scripts/index.js
+++ dist/scripts/index.js
@@ -1,6 +1,53 @@
 "use strict";
 
-// tooltip
+function init() {
+  if (document.querySelector('.tooltip')) {
+    var _tooltips = document.querySelectorAll('.tooltip');
+
+    _tooltips.forEach(function (tooltip) {
+      var instance = new Tooltip(tooltip);
+      instance.init();
+    });
+  }
+
+  if (document.querySelector('.tabs')) {
+    var _tabs = new Tabs(document.querySelector('.tabs'));
+
+    _tabs.init();
+  }
+
+  if (document.querySelector('.dropdown')) {
+    var _dropdowns = document.querySelectorAll('.dropdown');
+
+    _dropdowns.forEach(function (dropdown) {
+      var instance = new Dropdown(dropdown);
+      instance.init();
+    });
+  }
+
+  if (document.querySelector('.mainMenu')) {
+    var _show = function _show() {
+      _mainMenu.style.display = 'flex';
+      _mainMenu.style.top = '0';
+    };
+
+    var _close = function _close() {
+      _mainMenu.style.top = '-100%';
+    };
+
+    var _mainMenu = document.querySelector('.mainMenu');
+
+    var _closeMenu = document.querySelector('.closeMenu');
+
+    var _openMenu = document.querySelector('.openMenu');
+
+    _openMenu.addEventListener('click', _show);
+
+    _closeMenu.addEventListener('click', _close);
+  }
+} // tooltip
+
+
 var tooltips = document.querySelectorAll('.tooltip');
 tooltips.forEach(function (tooltip) {
   var instance = new Tooltip(tooltip);
