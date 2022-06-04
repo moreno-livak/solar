0ae305cfd0d0cee011e985fd4de3fcac3f447650
Moreno <moreno.livak@gmail.com>
2022-06-04 12:35:55 +0200

    ain

--- src/swup.js
+++ src/swup.js
@@ -1,38 +1,6 @@
 
 // Swup Reload Function
 function init() {
-
-  function getPosition(el) {
-    var xPos = 0;
-    var yPos = 0;
-    
-    while (el) {
-    if (el.tagName == "BODY") {
-        // deal with browser quirks with body/window/document and page scroll
-        var xScroll = el.scrollLeft || document.documentElement.scrollLeft;
-        var yScroll = el.scrollTop || document.documentElement.scrollTop;
-    
-        xPos += (el.offsetLeft - xScroll + el.clientLeft);
-        yPos += (el.offsetTop - yScroll + el.clientTop);
-    } else {
-        // for all other non-BODY elements
-        xPos += (el.offsetLeft - el.scrollLeft + el.clientLeft);
-        yPos += (el.offsetTop - el.scrollTop + el.clientTop);
-    }
-    
-    el = el.offsetParent;
-    }
-    return {
-        x: xPos,
-        y: yPos
-    };
-  }
-
-  function scrollToPlace(){ 
-    let myElement = document.querySelector("#need-to-scroll-here"); // Here is the element that we want to scroll to 
-    let position = getPosition(myElement);     
-    pageWrapper.scrollTo(0, position.y);
-  }
   
   if (document.querySelector('.tooltip')) {
     const tooltips = document.querySelectorAll('.tooltip');
